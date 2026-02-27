a12984cea1ac7ebc32fe79f71c528b53263b221f
Aditya <adityakhambekar9@gmail.com>
2026-02-26 23:31:55 -0600

    fix led rgb issue

--- src/main/java/org/team4639/frc2026/constants/led/Patterns.java
+++ src/main/java/org/team4639/frc2026/constants/led/Patterns.java
@@ -6,47 +6,32 @@ import edu.wpi.first.wpilibj.util.Color;
 import org.team4639.lib.led.pattern.LEDPattern;
 import org.team4639.lib.led.pattern2.BlockPattern;
 import org.team4639.lib.led.pattern2.MovingPattern;
+import org.team4639.lib.led.pattern2.RGBPattern;
 
 public class Patterns {
-    public static final LEDPattern DEFAULT = LEDPattern.ofColor(Color.kOrange);
-    public static final LEDPattern SHOOTER_REQUESTED = LEDPattern.ofColor(Color.kBlue);
-    public static final int MOVING_BLOCK_SIZE = 6;
-    public static final int LEDS_PER_SECOND = 30;
-    public static final LEDPattern SHOOTER_REQUESTED_AND_INTAKE = new MovingPattern(
-            new BlockPattern(
-                    MOVING_BLOCK_SIZE,
-                    SHOOTER_REQUESTED,
-                    LEDPattern.BLANK
-            ),
-            LEDS_PER_SECOND
-    );
-    public static final LEDPattern SHOOTING = LEDPattern.ofColor(Color.kGreen);
-    public static final LEDPattern SHOOTING_AND_INTAKE = new MovingPattern(
-            new BlockPattern(
-                    MOVING_BLOCK_SIZE,
-                    SHOOTING,
-                    LEDPattern.BLANK
-            ),
-            LEDS_PER_SECOND
-    );
-    public static final LEDPattern DEFAULT_INTAKE = new MovingPattern(
-            new BlockPattern(
-                    MOVING_BLOCK_SIZE,
-                    DEFAULT,
-                    LEDPattern.BLANK
-            ),
-            LEDS_PER_SECOND
-    );
+    private static final int MOVING_BLOCK_SIZE = 6;
+    private static final int LEDS_PER_SECOND = 30;
 
-    public static final LEDPattern PASSING = LEDPattern.ofColor(Color.kWhite);
-    public static final LEDPattern PASSING_AND_INTAKE = new MovingPattern(
-            new BlockPattern(
-                    MOVING_BLOCK_SIZE,
-                    PASSING,
-                    LEDPattern.BLANK
-            ),
-            LEDS_PER_SECOND
-    );
+    public static final LEDPattern DEFAULT = RGBPattern.GRB(Color.kOrange);
+    public static final LEDPattern SHOOTER_REQUESTED = RGBPattern.GRB(Color.kBlue);
+    public static final LEDPattern SHOOTER_REQUESTED_AND_INTAKE = createMovingPattern(SHOOTER_REQUESTED);
+    public static final LEDPattern SHOOTING = RGBPattern.GRB(Color.kGreen);
+    public static final LEDPattern SHOOTING_AND_INTAKE = createMovingPattern(SHOOTING);
+    public static final LEDPattern DEFAULT_INTAKE = createMovingPattern(DEFAULT);
 
-    public static final LEDPattern MANUAL = LEDPattern.ofColor(Color.kViolet);
+    public static final LEDPattern PASSING = RGBPattern.GRB(Color.kWhite);
+    public static final LEDPattern PASSING_AND_INTAKE = createMovingPattern(PASSING);
+
+    public static final LEDPattern MANUAL = RGBPattern.GRB(Color.kViolet);
+
+    private static LEDPattern createMovingPattern(LEDPattern pattern) {
+        return new MovingPattern(
+                new BlockPattern(
+                        MOVING_BLOCK_SIZE,
+                        pattern,
+                        LEDPattern.BLANK
+                ),
+                LEDS_PER_SECOND
+        );
+    }
 }
